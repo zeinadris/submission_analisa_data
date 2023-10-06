4bf658681c81ed6a8f448fd6afef7e90daeb4399
Zein Adri <zeinadri@gmail.com>
2023-10-06 14:15:49 +0800

    first commit

---
+++ dashboard/dashboard.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+class DataAnalyzer:
+    def __init__(self, df):
+        self.df = df
+    
+    def create_sum_spend_df(self):
+        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
+            "payment_value": "sum"
+        })
+        sum_spend_df = sum_spend_df.reset_index()
+        sum_spend_df.rename(columns={
+            "payment_value": "total_spend"
+        }, inplace=True)
+
+        return sum_spend_df
+
+    def create_sum_order_items_df(self):
+        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+        sum_order_items_df.rename(columns={
+            "product_id": "product_count"
+        }, inplace=True)
+        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+
+        return sum_order_items_df
+
+    def create_bystate_df(self):
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+        return bystate_df, most_common_state
+
+    def create_bycity_df(self):
+        bycity_city = self.df.groupby(by="customer_city").customer_id.nunique().reset_index()
+        bycity_city.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_city = bycity_city.loc[bycity_city['customer_count'].idxmax(), 'customer_city']
+        bycity_city = bycity_city.sort_values(by='customer_count', ascending=False).head(10)
+
+        return bycity_city, most_common_city
+
+
+
+# Dataset
+datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df = pd.read_csv('all_data.csv')
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+for col in datetime_cols:
+    all_df[col] = pd.to_datetime(all_df[col])
+
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
+
+# Sidebar
+with st.sidebar:
+    # Title
+    st.title("Zein Adri Saputro")
+
+    # Logo Image
+    st.image("https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg")
+
+    # Date Range
+    start_date, end_date = st.date_input(
+        label="Select Date Range",
+        value=[min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+)
+
+# Main
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
+                 (all_df["order_approved_at"] <= str(end_date))]
+
+function = DataAnalyzer(main_df)
+sum_spend_df = function.create_sum_spend_df()
+sum_order_items_df = function.create_sum_order_items_df()
+state, most_common_state = function.create_bystate_df()
+city, most_common_city = function.create_bycity_df()
+
+# Title
+st.header("E-Commerce Dashboard :convenience_store:")
+
+# Order Items
+st.subheader("Order Items")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Total Items: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Average Items: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis ='x', labelsize=30)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+# Customer Spend Money
+st.subheader("Customer Spend Money")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Total Spend: **{total_spend}**")
+
+with col2:
+    avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
+    st.markdown(f"Average Spend: **{avg_spend}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    sum_spend_df["order_approved_at"],
+    sum_spend_df["total_spend"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Customer Demographic
+st.subheader("Customer Demographic")
+tab1, tab2 = st.tabs(["State", "City"])
+
+with tab1:
+    most_common_state = state.customer_state.value_counts().index[0]
+    st.markdown(f"Most Common State: **{most_common_state}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=state.customer_state.value_counts().index,
+                y=state.customer_count.values, 
+                data=state,
+                palette=["#068DA9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
+                    )
+
+    plt.title("Number customers from State", fontsize=15)
+    plt.xlabel("State")
+    plt.ylabel("Number of Customers")
+    plt.xticks(fontsize=12)
+    st.pyplot(fig)
+with tab2:
+    most_common_city = city.customer_city.value_counts().index[0]
+    st.markdown(f"Most Common city: **{most_common_city}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=city.customer_city.value_counts().index,
+                y=city.customer_count.values,
+                data=city, 
+                palette=["#068DA9" if score == most_common_city else "#D3D3D3" for score in city.customer_city.value_counts().index]
+                    )
+
+    plt.title("Number customers from City", fontsize=15)
+    plt.xlabel("City")
+    plt.ylabel("Number of Customers")
+    plt.xticks(rotation=45 ,fontsize=12)
+    st.pyplot(fig)
+
+
+    st.caption('Copyright (C) Zein Adri Saputro 2023')
+
